84cd9b5b459a4f55b0a565eb9a61a4cffd4c0298
Jakov <jakov@MacBook-Pro-Jakov.local>
2022-06-18 21:30:31 +0300

    1 способ для работы с состояниями

--- src/shared/StateExamples/StarWarsNameFunction/StarWarsNameFunction.tsx
+++ src/shared/StateExamples/StarWarsNameFunction/StarWarsNameFunction.tsx
@@ -6,13 +6,20 @@ export function StarWarsNameFunction() {
   const randomName = () => uniqueNamesGenerator({dictionaries: [starWars], length: 1 });
 
   // Это хук. Хуки можно использовать только в функциональных компонентах.
+  // name - состояние, setName - функция
   const [name, setName] = React.useState(randomName);
-  
+  const [count, setCount] = React.useState(0);
+  const handleClick = () => { 
+    setName(randomName);
+    setCount((prevCount) => prevCount + 1);
+  };
+  console.log('>>', count);
+
   return(
     <section>
       <span>{name}</span>
       <div />
-      <button>Мне нужно имя!</button>
+      <button onClick={handleClick}>Мне нужно имя!</button>
     </section>
   )
 }
