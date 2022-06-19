50e0a52eaa5afceb3e969c4d6edb7196d178e59d
Jakov <jakov@MacBook-Pro-Jakov.local>
2022-06-19 21:30:09 +0300

    Начальная настрока

---
+++ cfg/webpack.server.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+  target: "node",
+  mode: NODE_ENV ? NODE_ENV : 'development',
+  entry: path.resolve(__dirname, '../src/server/server.js'),
+  output: {
+    path: path.resolve(__dirname, '../dist/server'),
+    filename: "server.js"
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [{
+        // Регулярное выражение, в котором описываем рассширения файлов, которые мы собираемся обрабатывать.
+        test: /\.[jt]sx?$/,
+
+        // Даем команду, что для обработки нужно использовать ts-loader.
+        use: ['ts-loader']
+    }]
+  },
+  optimization: {
+    minimize: false,
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,9 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const clientConfig = require('./cfg/webpack.client.config');
+const serverConfig = require('./cfg/webpack.server.config');
 
-const NODE_ENV = process.env.NODE_ENV;
-const IS_DEV = NODE_ENV === 'development';
-const IS_PROD = NODE_ENV === 'production';
-
-function setupDevtool() {
-    if(IS_DEV) return 'eval';
-    if(IS_PROD) return false;
-}
-
-module.exports = {
-    // Для обучения резолвить другие расширения.
-    resolve:{
-        extensions:['.js', '.jsx', '.ts', '.tsx', '.json']
-    },
-
-    // Значение mode принимает строку, в которой будет значение либо development либо production
-    // Чтобы не хардкодить, а решать это динамически, воспользуемся env переменными.
-    mode: NODE_ENV ? NODE_ENV : 'development',
-     
-    // Указываем откуда начинаем.
-    // Указываем __dirname - что это такое? <<<<<<<<<-----------------------------------
-    // И название файла с которого будет все начинаться. 
-    entry: path.resolve(__dirname, 'src/index.jsx'),
-    
-    // Указываем куда складывать все файлы, которые он переработает (странспилирует.)
-    // output - это объект, внутри которого может существовать множество настроек.
-    // path - описываем абсолютный путь до папки dist
-    // filename - называем наш бандел index.js
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
-    },
-
-    // loader - это специальные пакеты, которые позволяют webpacku 
-    // преобразовывать файлы одного типа в файлы другого типа.
-    // По умолчанию webpack может работать с файлами JS and json, а другие расширение не понятны.
-    // Это объект, внутри которого вызываем свойство rules - является массивом.
-    // Внутри массива мы и будем настраивать все loaderi которые используем.
-    // Для минимальной настрокий loadera необходимы 2 свойства test and .
-    module: {
-        rules: [{
-            // Регулярное выражение, в котором описываем рассширения файлов, которые мы собираемся обрабатывать.
-            test: /\.[tj]sx?$/,
-
-            // Даем команду, что для обработки нужно использовать ts-loader.
-            use: ['ts-loader']
-        }]
-    },
-
-    // Для копирования файла и модификации этого файла на лету.
-    plugins:[
-        new HTMLWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })
-    ],
-    
-    // Чтобы подлялся, минимально неоходимо 3 настройки.
-    devServer: {
-        port: 3000,
-        open: true,
-        
-        // Перезагружает странциу когда что-то меняется в файлах.
-        hot: IS_DEV
-    },
-
-    // Фирмируются ли и как формируются сорцмапы.
-    devtool: setupDevtool()
-};
+// Если мы передаем массив, тогда webpack исполняет каждую кофигурацию и
+// генерит для нее соответствующий бандл.
+module.exports = [
+  clientConfig,
+  serverConfig,
+];
